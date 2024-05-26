26994ae7eaca75bbf4d846fa3fcc24055a7e999b
saurabh <saurbabh@gmail.com>
2024-05-26 17:10:25 +0530

    rg added

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "Sharma" {
+  name     = "saurabh"
+  location =  "eastus"
+}
+resource "azurerm_resource_group" "Sharma1" {
+  name     = "myrg"
+  location =  "japanwest"
+}
