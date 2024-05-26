5646a183319c941a916a0e7acf3129f8ddd34e88
saurabh <saurbabh@gmail.com>
2024-05-26 17:27:28 +0530

    location changed

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "Sharma" {
   name     = "saurabh"
-  location =  "eastus"
+  location =  "japaneast"
 }
 resource "azurerm_resource_group" "Sharma1" {
   name     = "myrg"
-  location =  "japanwest"
+  location =  "central india"
 }
