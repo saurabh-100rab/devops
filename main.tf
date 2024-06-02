84d28acdf297e38ce83c4535caa9834fc9cef8df
saurabh-100rab <162677892+saurabh-100rab@users.noreply.github.com>
2024-06-02 13:04:44 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "Sharma" {
-  name     = "saurabh11"
-  location =  "south india"
-}
-resource "azurerm_resource_group" "Sharma1" {
-  name     = "myrg1"
-  location =  "south india"
-}
